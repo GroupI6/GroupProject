b201ead59e95dd6832916f4e9c549ed0da907f14
GroupI6 <kings1@cs.man.ac.uk>
2013-02-27 13:23:22 +0000

    Update ConfirmationInterface.java

--- ConfirmationInterface.java
+++ ConfirmationInterface.java
@@ -14,16 +14,11 @@ import java.awt.Component;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
-
 // Program to implement a user interface.
-public class CommonInterface extends JFrame implements ActionListener
+public class ConfirmationInterface extends JFrame implements ActionListener
 {
   // JButtons for use on the interface.
-  private JButton passengerSelect = new JButton("Passenger");
-  private JButton driverSelect = new JButton("Driver");
-  private JButton controllerSelect = new JButton("Controller");
-  private JButton homeSelect = new JButton("Home");
-
+  private JButton backButton = new JButton("Back");
   
   // A public method to set the heading
   private static JPanel setHeading()
@@ -38,7 +33,7 @@ public class CommonInterface extends JFrame implements ActionListener
     // JLabel for the company name
     JLabel companyName = new JLabel("GMPTE");
     JLabel productName = new JLabel("Integrated Bus Management System (IBMS)");
-    
+     JLabel loginName ;
     // Change fonts etc to match logo
     companyName.setFont(new Font("Serif", Font.BOLD, 32));
     companyName.setForeground(Color.black);
@@ -46,10 +41,19 @@ public class CommonInterface extends JFrame implements ActionListener
     productName.setFont(new Font("Serif", Font.BOLD, 16));
     productName.setForeground(Color.black);
     
+    LoginInterface theLoginInterface = new LoginInterface();
+    
+    loginName= new JLabel(theLoginInterface.loggedName()) ;
+    
+    loginName.setFont(new Font("Serif", Font.BOLD, 18));
+    loginName.setForeground(Color.black);
+    
+ 
     // First element for the companyHeaderPanel
     companyHeaderPanel.add(companyName);
     companyHeaderPanel.add(productName);
-    
+    if (theLoginInterface.connected()==true)
+      companyHeaderPanel.add(loginName);
     return(companyHeaderPanel);
   } // setHeading
   
@@ -75,78 +79,98 @@ public class CommonInterface extends JFrame implements ActionListener
   } // makeButton
   
   // Firstly, we need a constructor to build the GUI.
-  public CommonInterface()
+  public ConfirmationInterface()
   {
     // Set the window title
-    setTitle("IBMS: Main");
+    setTitle("IBMS: Confirm Request");
     
     // Make contents appear.
     Container contents = getContentPane();
     
-    // Pixel gaps are 10 between each column and 20 between each row.
-    contents.setLayout(new BorderLayout());
-    contents.setBackground(Color.WHITE);    
-    
-    // Add the heading panel
+    // Set the heading
     contents.add(setHeading(), BorderLayout.NORTH);
-    
+           
     // Make another JPanel for the buttons - separate section of the screen.
     JPanel mainPanel = new JPanel();
     
     // Add mainPanel to the layout and format
     contents.add(mainPanel, BorderLayout.CENTER);
-    mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
+    mainPanel.setLayout(new GridLayout(0, 1));
     mainPanel.setBackground(Color.white);
     
-     // Format the buttons
-    passengerSelect = makeButton(passengerSelect, controllerSelect);
-    driverSelect = makeButton(driverSelect, controllerSelect);
-    controllerSelect = makeButton(controllerSelect, controllerSelect);
-    homeSelect= makeButton(homeSelect,controllerSelect);
+    // Format the buttons
+    backButton = makeButton(backButton, backButton);
 
+    // Add a JLabel for the confirmation
+   
+    JLabel confirmationJLabel; 
+    LoginInterface theLoginInterface = new LoginInterface();
+    RequestHolidayInterface theRequestHolidayInterface = new RequestHolidayInterface();
+    ViewRequestsInterface theViewRequestsInterface = new ViewRequestsInterface();
+    
+     if (theViewRequestsInterface.approved() == 1)
+         confirmationJLabel = new JLabel("You have approved request ");
+      else if(theViewRequestsInterface.approved() == 0)
+         confirmationJLabel = new JLabel("You have cancelled request ");
+     else
+      {
+     if (theLoginInterface.connected()==false)
+       confirmationJLabel = new JLabel("Your login and password details are wrong ");
+     else
+       {
+         if (theRequestHolidayInterface.requestedDays()==true)
+         {
+            confirmationJLabel = new JLabel("You have submitted your request for approval");
+          
+         }
+         else
+           confirmationJLabel = new JLabel("This is invalid request");
+       }
+      }
+    mainPanel.add(confirmationJLabel);
     // Add JButtons to layout
-    mainPanel.add(passengerSelect);
-    passengerSelect.addActionListener(this);
+    mainPanel.add(backButton);
+    backButton.addActionListener(this);
     mainPanel.add(Box.createRigidArea(new Dimension(10, 10)));
-    mainPanel.add(driverSelect);
-    driverSelect.addActionListener(this);
-    mainPanel.add(Box.createRigidArea(new Dimension(10, 10)));
-    mainPanel.add(controllerSelect);
-    controllerSelect.addActionListener(this);
-    mainPanel.add(Box.createRigidArea(new Dimension(10, 10)));
-  //  mainPanel.add(homeSelect);
-   // homeSelect.addActionListener(this);
-    //mainPanel.add(Box.createRigidArea(new Dimension(2, 10)));
+
     
     // Set the default close operation and make the correct size.
     setDefaultCloseOperation(EXIT_ON_CLOSE);
     pack();
     setLocationRelativeTo(null);
-  } // CommonInterface
+  } // ConfirmationInterface
   
   // Action performed method
-  public void actionPerformed(ActionEvent event)
+  public void actionPerformed(ActionEvent thisEvent)
   {
-    if(event.getSource() == passengerSelect)
-      System.out.println("I pressed the passenger button");
-    
-    else if (event.getSource() == driverSelect)
+  //database.openBusDatabase();
+    if(thisEvent.getSource() == backButton)
+     {
+   ViewRequestsInterface theViewRequestsInterface = new ViewRequestsInterface();
+   if(theViewRequestsInterface.approved() != -1)
     {
-       LoginInterface theLoginInterface = new LoginInterface();
-      theLoginInterface.setVisible(true);
+      ControllerInterface theControllerInterface = new ControllerInterface();
+      theControllerInterface.setVisible(true); 
+this.dispose(); 
+   }
+else
+{
+      LoginInterface theLoginInterface = new LoginInterface();
+     if (theLoginInterface.connected()==false)
+    {
+   theLoginInterface.setVisible(true);
+   this.dispose();
+   }
+
+     else 
+{
+      DriverInterface theDriverInterface = new DriverInterface();
+      theDriverInterface.setVisible(true);
       this.dispose();
-    } // if
-      
-    else if (event.getSource() == controllerSelect)
-      System.out.println("I pressed the controller button");
+} 
+
+}
+   }
   } // actionPerformed
-  
-  public static void main(String[] args)
-  {
-    // Run the window.
-    CommonInterface theCommonInterface = new CommonInterface();
-    
-    // Make sure we can see it.
-    theCommonInterface.setVisible(true);
-  } // main
-} // class CommonInterface
+
+} // class ConfirmationInterface
